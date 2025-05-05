537338d60ac368d8544e26e8ad122b568accb2b3
ddhanawade <ddhanawade804@gmail.com>
2025-05-06 00:06:13 +0530

    Update VehicleNotFoundException.java

--- src/main/java/com/inventory/fleet_manager/exception/VehicleNotFoundException.java
+++ src/main/java/com/inventory/fleet_manager/exception/VehicleNotFoundException.java
@@ -1,5 +1,9 @@
 package com.inventory.fleet_manager.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class VehicleNotFoundException extends RuntimeException {
     public VehicleNotFoundException(String message) {
         super(message);
