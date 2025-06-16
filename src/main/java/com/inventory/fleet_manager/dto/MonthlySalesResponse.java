b132797da626ce6c1f7533108575fec0cb578134
ddhanawade <deepak_dhanawade@intuit.com>
2025-06-17 01:45:10 +0530

    reporting code changes

--- src/main/java/com/inventory/fleet_manager/dto/MonthlySalesResponse.java
+++ src/main/java/com/inventory/fleet_manager/dto/MonthlySalesResponse.java
@@ -1,16 +1,16 @@
 package com.inventory.fleet_manager.dto;
 
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.Date;
 
 @Data
 @RequiredArgsConstructor
 public class MonthlySalesResponse {
     private String model;
-    private LocalDate orderDate;
+    //private LocalDate orderDate;
     private String location;
     private double invoiceValue;
     private String make;
@@ -20,4 +20,20 @@ public class MonthlySalesResponse {
     private String keyNumber;
     private String interest;
     private String status;
+
+    private Long orderId;
+    private String customerName;
+    private String phoneNumber;
+    private String leadName;
+    private  String salesPersonName;
+    private LocalDate orderDate;
+    private LocalDate deliveryDate;
+    private String financerName;
+    private String financeType;
+    private String remarks;
+    private LocalDate createdAt;
+    private LocalDate updatedAt;
+    private String createdBy;
+    private String updatedBy;
+    private String orderStatus;
 }
